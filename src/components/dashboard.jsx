711e6c72bf093a12d35230f790b6294099b81a68
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2020-08-22 13:34:22 +0200

    make dashboard a functionnal comp

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -8,37 +8,35 @@ import Projects from "./projects";
 import Formation from "./formation";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 
-class Dashboard extends React.Component {
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <div className="title">
-                            <h1>Smarzyk Mathieu</h1>
-                            <hr />
-                            <small>Développeur Web</small>
-                        </div>
+const Dashboard = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <div className="title">
+            <h1>Smarzyk Mathieu</h1>
+            <hr />
+            <small>Développeur Web</small>
+          </div>
 
-                        <div>
-                            <Menu />
-                        </div>
+          <div>
+            <Menu />
+          </div>
 
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route path="/Home" component={Home} />
-                            <Route path="/Abilities" component={Abilities} />
-                            <Route path="/Projects" component={Projects} />
-                            <Route path="/Experience" component={Experience} />
-                            <Route path="/Formation" component={Formation} />
-                            <Route path="/Hobbies" />
-                            <Route path="/Contact" component={Contact} />
-                        </Switch>
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
-}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Home" component={Home} />
+            <Route path="/Abilities" component={Abilities} />
+            <Route path="/Projects" component={Projects} />
+            <Route path="/Experience" component={Experience} />
+            <Route path="/Formation" component={Formation} />
+            <Route path="/Hobbies" />
+            <Route path="/Contact" component={Contact} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default Dashboard;
